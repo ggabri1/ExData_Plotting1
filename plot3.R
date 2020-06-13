3eb3141d1a5f8accbe93925fe174b691995b0611
Gnel Gabrielyan <65758538+ggabri1@users.noreply.github.com>
2020-06-13 02:33:03 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -38,6 +38,6 @@ legend("topright", col = c("black", "red", "blue"),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.width = strwidth("100,000,000,000,000,000"),
        lty = 1:1, pt.cex = 3, cex=1.1)
 
-dev.copy(png, file = "rplot3.png", width = 480, height = 480, units = "px")
+dev.copy(png, file = "plot3.png", width = 480, height = 480, units = "px")
 dev.off()
 ##saving the plot as a *.png file
